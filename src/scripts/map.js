bb643204829de9e104f0dac08fca892fdd979308
Diana Nagimova <12diana13@gmail.com>
2023-02-12 23:11:59 +0300

    fix bags

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -15,7 +15,7 @@ const init = () => {
 const myCollection = new ymaps.GeoObjectCollection({},{
   draggable: false, 
   iconLayout: 'default#image',
-  iconImageHref: '../src/img/marker.svg',
+  iconImageHref: './img/marker.svg',
   iconImageSize: [46, 58],
   iconImageOffset: [-35, -52]
 });
