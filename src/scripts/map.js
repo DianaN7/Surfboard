8caacd9ac29f9ea96983a32f9b565ae37734d6e6
Diana Nagimova <12diana13@gmail.com>
2023-02-12 22:15:07 +0300

    marker

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -15,7 +15,7 @@ const init = () => {
 const myCollection = new ymaps.GeoObjectCollection({},{
   draggable: false, 
   iconLayout: 'default#image',
-  iconImageHref: "src/img/marker.svg",
+  iconImageHref: ('xlink:href', '../img/sprite.svg#marker'),
   iconImageSize: [46, 58],
   iconImageOffset: [-35, -52]
 });
